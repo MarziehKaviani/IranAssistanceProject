0af3621aea6c2dde7a51bdfe20f87edde6590574
MarziehKaviani <itsjayran@gmail.com>
2024-10-22 10:48:34 +0330

    swagger added

--- IranAssistanceProject/urls.py
+++ IranAssistanceProject/urls.py
@@ -1,22 +1,42 @@
-"""
-URL configuration for IranAssistanceProject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
+from django.utils.translation import gettext_lazy as _
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+
+swagger_description = """
+### IranAssistanceProject APIs
+  
+    `Summery`: This API receives bulk insurance data from various insurance providers (e.g., Pasargad, Hekmat, Mellat, Saman, etc.).
+    The providers submit a unified JSON containing personal, insurance, policyholder, and plan details. The API validates and processes the data,
+    ensuring compliance with our business rules. Each section, such as personal details, insurance provider info, policyholder details,
+    insurance policy, and plan specifics, follows a strict format and sequence. Validation includes email, mobile number, national ID,
+    and required fields. The API supports custom field names for different providers while adhering to business logic. Proper error handling
+    and validation messages are implemented.
+
+
+`_________________________________________________________________`
+    
+"""
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="APIs",
+        default_version="v1",
+        description=swagger_description,
+        contact=openapi.Contact(email=""),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('api/', include('core.urls')),
 ]
